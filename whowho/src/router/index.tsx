27f39f62e3772adbd3952520a03a86bdc8780c3f
Yongjin081002 <yg4059540@gmail.com>
2026-02-22 03:23:09 +0900

    feat: detail feed page view

--- whowho/src/router/index.tsx
+++ whowho/src/router/index.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from '../pages/Home'
 import Create from  '../pages/Create/Create'
 import ReturnList from '../pages/ReturnList'
+import DetailFeed from '../pages/DetailFeed'
 
 
 export default function AppRouter() {
@@ -11,6 +12,8 @@ export default function AppRouter() {
         <Route path="/" element={<Home />} />
         <Route path="/create" element={<Create />} />
         <Route path="/list" element={<ReturnList />} />
+        <Route path="/detail" element={<ReturnList />} />
+        <Route path="/feed/:id" element={<DetailFeed />} />
       </Routes>
     </BrowserRouter>
   )
