cd943bf9dfb27fdc330469881eed7ac51687d72e
Yongjin081002 <yg4059540@gmail.com>
2026-02-22 00:16:37 +0900

    style: header component

--- whowho/src/components/header/style.ts
+++ whowho/src/components/header/style.ts
@@ -1,4 +1,9 @@
+import type React from "react"
+
 export const wrapperStyle: React.CSSProperties = {
   width: '100%',
   height: '46px',
+  position: "fixed",
+  top:"30px",
+  paddingLeft:"40px",
 }
